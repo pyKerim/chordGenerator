2b9b843d20fa1b835c6591aa183d12ed4bc50efb
Kerim Yusuf <kerimyusuf63@gmail.com>
2025-12-14 19:18:43 +0000

    added minor chords, saves score now from last game

--- chord_generator.py
+++ chord_generator.py
@@ -1,28 +1,25 @@
 import random
-from chords import chords, enharmonics
-import input_utils
 
+from chords import chords
 from input_utils import input_normalisation
-
-
 
 # Print title
 print("Welcome to the Chord Quiz")
-print("\n")
+input("Press any key")
+print("\n" * 25)
 
-# Loop the quiz for 5 questions
+# Loop the quiz till all lives are gone
+lives = 5
 question_number = 1
 score = 0
-while question_number <= 5:
+
+while lives > 0:
     # Randomly select a chord from the dictionary
     chord, chord_tones = random.choice(list(chords.items()))
-
     # Get the answer as string and split the letters
+    print(f"Question #{question_number}: {chord}")
     answer = input(f"What are the tones in {chord}:\n")
     answer = input_normalisation(answer)
-
-    print("User:", answer)
-    print("Expected:", chords[chord])
 
     # Check if the lists match
     if set(answer) == set(chords[chord]) and len(answer) == len(chords[chord]):
@@ -30,5 +27,11 @@ while question_number <= 5:
         print("Correct!")
     else:
         print(f"Incorrect! The correct answer was: {', '.join(chords[chord])}")
+        lives -= 1
+        print(f"You have {lives} lives left.")
     question_number += 1
-print(f"You scored {score}/5!")
+
+print(f"You scored {score}")
+my_scores = open("scores.txt", "a")
+my_scores.write(str(score))
+my_scores.close()
--- chords.py
+++ chords.py
@@ -2,14 +2,20 @@ chords = {
     "C Major": ["C", "E", "G"],
     "F Major": ["F", "A", "C"],
     "Bb Major": ["Bb", "D", "F"],
-    "Eb Major": ["Eb", "G" , "B"],
+    "Eb Major": ["Eb", "G", "B"],
     "Ab Major": ["Ab", "C", "Eb"],
-    "Db Major": ["Db", "F", "Ab"]
+    "Db Major": ["Db", "F", "Ab"],
+    "A  Minor": ["A", "C", "E"],
+    "D Minor": ["D", "F", "A"],
+    "G Minor": ["G", "Bb", "D"],
+    "C Minor": ["C", "Eb", "G"],
+    "F Minor": ["F", "Ab", "C"],
+    "Bb Minor": ["Bb", "Db", "F"],
 }
 
 enharmonics = {
-    "A#":"Bb",
-    "B#":"C",
+    "A#": "Bb",
+    "B#": "C",
     "C#": "Db",
     "D#": "Eb",
     "E#": "F",
