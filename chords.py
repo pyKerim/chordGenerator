a9636d3575346e096a2290bb0492be1ac3a788be
Kerim Yusuf <kerimyusuf63@gmail.com>
2025-12-17 20:58:41 +0000

    Added some seventh chords

--- chords.py
+++ chords.py
@@ -11,6 +11,10 @@ chords = {
     "C Minor": {"notes": ["C", "Eb", "G"], "type": "Minor"},
     "F Minor": {"notes": ["F", "Ab", "C"], "type": "Minor"},
     "Bb Minor": {"notes": ["Bb", "Db", "F"], "type": "Minor"},
+    "C7": {"notes": ["C", "E", "G", "Bb"], "type": "Seventh"},
+    "F7": {"notes": ["F", "A", "C", "Eb"], "type": "Seventh"},
+    "Bb7": {"notes": ["Bb", "D", "F", "Eb"], "type": "Seventh"},
+    "Eb7": {"notes": ["Eb", "G", "Bb", "Db"], "type": "Seventh"},
 }
 
 enharmonics = {
